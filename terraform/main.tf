da076f384ef218168de5ac2d65075e65c5c03589
Ewen Michel <ewen.michel@soprasteria.com>
2025-04-14 13:28:54 +0200

    Added tfstate to azure backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "devops-rg"
+    storage_account_name = "tfstate16669"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 }
