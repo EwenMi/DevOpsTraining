60954e57230116774340ea066eeca9078d64f808
EwenMi <ewen.michel@soprasteria.com>
2025-04-14 12:19:10 +0200

    modified ssh management

--- terraform/main.tf
+++ terraform/main.tf
@@ -118,6 +118,6 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file(var.ssh_public_key_path)
+    public_key = public_key = var.ssh_public_key
   }
 }
